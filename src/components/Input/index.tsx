6b80284feb02d4bb71840d6a23badfb60dc57186
feijohigor <feijohigor@gmail.com>
2023-09-25 13:06:40 -0300

    adding validation on signup and signin forms

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,22 +1,79 @@
+import { forwardRef, useEffect, useRef, useImperativeHandle, useState, useCallback } from 'react'
 import { Container, TextInput, Icon } from './styles'
 import { TextInputProps } from 'react-native'
+import { useField } from '@unform/core'
 
 interface InputProps extends TextInputProps {
     name: string
     icon: string
 }
 
-const Input = ({ name, icon, ...rest }: InputProps) => {
+interface InputValueReference {
+    value: string
+}
+
+interface InputRef {
+    focus(): void
+}
+
+const Input: React.ForwardRefRenderFunction<InputRef, InputProps> = ({ name, icon, ...rest}, ref) => {
+    const inputElementRef = useRef<any>(null)
+
+    
+    const { registerField, defaultValue='', fieldName, error } = useField(name)
+    const inputValueRef = useRef<InputValueReference>({ value: defaultValue })
+    
+    const [isFocused, setIsFocused] = useState(false)
+    const [isFilled, setIsFilled] = useState(false)
+
+    useImperativeHandle(ref, () => ({
+        focus() {
+            inputElementRef.current.focus()
+        }
+    }))
+
+    useEffect(() => {
+        registerField<string>({
+            name: fieldName,
+            ref: inputValueRef.current,
+            path: 'value',
+            setValue(ref: any, value) {
+                inputValueRef.current.value = value
+                inputElementRef.current.setNativeProps({ text: value })
+            },
+            clearValue() {
+                inputValueRef.current.value = ''
+                inputElementRef.current.clear()
+            }
+        })
+    }, [fieldName, registerField])
+    
+    const handleInputFocus = useCallback(() => {
+        setIsFocused(true)
+    }, [])
+
+    const handleInputBlur = useCallback(() => {
+        setIsFocused(false)
+        
+        setIsFilled(!!inputValueRef.current.value)
+    }, [])
+
+
     return (
-        <Container>
-            <Icon name={icon} size={20} color={'#666360'} />
+        <Container isFocused={isFocused} isErrored={!!error}>
+            <Icon name={icon} size={20} color={isFocused || isFilled ? '#ff9000' : '#666360'} />
             <TextInput
+                ref={inputElementRef}
                 keyboardAppearance='dark'
                 placeholderTextColor={'#666360'}
+                defaultValue={defaultValue}
+                onFocus={handleInputFocus}
+                onBlur={handleInputBlur}
+                onChangeText={value => inputValueRef.current.value = value}
                 {...rest}
             />
         </Container>
     )
 }
 
-export default Input
+export default forwardRef(Input)
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,17 +1,64 @@
-import { Image, KeyboardAvoidingView, Platform, View, ScrollView } from 'react-native'
+import { useRef, useCallback } from 'react'
+import { Image, KeyboardAvoidingView, Platform, View, ScrollView, TextInput, Alert } from 'react-native'
 import Icon from 'react-native-vector-icons/Feather'
 import { Container, Title, BackToSignIn, BackToSignInText } from './styles'
+import { Form } from '@unform/mobile'
+import { FormHandles } from '@unform/core'
 
 import Input from '../../components/Input'
 import Button from '../../components/Button'
 
 import logoImg from '../../assets/logo.png'
-import { ParamListBase, useNavigation } from '@react-navigation/native'
-import { NativeStackNavigationProp } from '@react-navigation/native-stack'
+import { useNavigation } from '@react-navigation/native'
+import * as Yup from 'yup'
+import getValidationErrors from '../../utils/getValidationErrors'
+
+interface SignUpFormData {
+    name: string
+    email: string
+    password: string
+}
 
 const SignUp = () => {
-
+    const formRef = useRef<FormHandles>(null)
     const { goBack } = useNavigation()
+
+    const emailInputRef = useRef<TextInput>(null)
+    const passwordInputRef = useRef<TextInput>(null)
+    
+    const handleSignUp = useCallback(async (data: SignUpFormData) => {
+        try {
+            formRef.current?.setErrors({})
+            const schema = Yup.object().shape({
+                name: Yup.string().required('Nome obrigatório'),
+                email: Yup.string().required('E-mail obrigatório').email('Digite um e-mail válido'),
+                password: Yup.string().min(6, 'No mínimo 6 digitos'),
+            })
+
+            await schema.validate(data, {
+                abortEarly: false,
+            })
+
+            // await signIn({
+            //     email: data.email,
+            //     password: data.password
+            // })
+
+            // navigate('/dashboard')
+        } catch(err) {
+            if(err instanceof Yup.ValidationError) {
+                const errors = getValidationErrors(err as Yup.ValidationError)
+                
+                formRef.current?.setErrors(errors)
+                return
+            }
+
+            Alert.alert(
+                'Erro na autenticação', 
+                'Ocorreu um erro ao fazer login, cheque as credenciais.'
+            )
+        }
+    }, [])
 
     return (
         <>
@@ -31,11 +78,43 @@ const SignUp = () => {
                             <Title>Crie sua conta</Title>
                         </View>
 
-                        <Input name='name' icon='user' placeholder='Nome' />
-                        <Input name='email' icon='mail' placeholder='E-mail' />
-                        <Input name='password' icon='lock' placeholder='Senha'/>
+                        <Form ref={formRef} onSubmit={handleSignUp}>
+                            <Input
+                                autoCapitalize='words'
+                                name='name' 
+                                icon='user' 
+                                placeholder='Nome' 
+                                returnKeyType='next'
+                                onSubmitEditing={() => {
+                                    emailInputRef.current?.focus()
+                                }}
+                            />
+                            <Input 
+                                ref={emailInputRef}
+                                keyboardType='email-address'
+                                autoCorrect={false}
+                                autoCapitalize='none'
+                                name='email' 
+                                icon='mail' 
+                                placeholder='E-mail' 
+                                returnKeyType='next'
+                                onSubmitEditing={() => {
+                                    passwordInputRef.current?.focus()
+                                }}
+                            />
+                            <Input 
+                                ref={passwordInputRef}
+                                textContentType='newPassword'
+                                secureTextEntry
+                                name='password' 
+                                icon='lock' 
+                                placeholder='Senha'
+                                returnKeyType='send'
+                                onSubmitEditing={() => formRef.current?.submitForm()}
+                            />
+                        </Form>
 
-                        <Button onPress={() => {}}>Entrar</Button>
+                        <Button onPress={() => formRef.current?.submitForm()}>Entrar</Button>
 
                     </Container>
                 </ScrollView>
